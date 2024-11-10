855801fabca79f2dca6255aa293c1089614c443a
ayushdwivedee <ayushbhartiya1494927@gmail.com>
2024-11-10 16:33:01 +0530

    1st step done

--- eval_js.js
+++ eval_js.js
@@ -5,31 +5,37 @@ let fetchedPosts = () => {
 };
 fetchedPosts();
 
-
 let mainDiv = document.getElementById("main");
 let myData = (data) => {
-
-data.map((el)=>{
-    let div=document.createElement("div")
-    let id = document.createElement("p")
-id.innerText = `ID:- ${el.id}`
-id.style.fontSize="24px"
-let h2 = document.createElement("p");
-h2.innerHTML = `Title:- ${el.title}`;
-h2.style.color="red"
-let para = document.createElement("p");
-para.innerHTML = `para:- ${el.body}`;
-div.append(id, h2, para);
-mainDiv.append(div)
- 
- 
-})
-  
-  
-  
-  
-  
-  
+  data.map((el) => {
+    let div = document.createElement("div");
+    let id = document.createElement("p");
+    id.innerText = `ID:- ${el.id}`;
+    id.style.fontSize = "24px";
+    let h2 = document.createElement("p");
+    h2.innerHTML = `Title:- ${el.title}`;
+    h2.style.color = "red";
+    let para = document.createElement("p");
+    para.innerHTML = `para:- ${el.body}`;
+    div.append(id, h2, para);
+    mainDiv.append(div);
+  });
 
   
 };
+
+// 2nd step
+let userPosts = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((data) => userData(data));
+};
+
+userData()
+
+userData.map((user)=>{
+    console.log("hg-",user)
+  let select=document.createElement("select")
+
+  select.innerHTML=`${user.name}`
+})
